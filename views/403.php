dea3a9ad2c1bf68218518d7e1723c73452c650ca
Watsi1984 <robertw@automatedanalytics.co.uk>
2026-07-03 11:22:06 +0100

    First Demo Commit

---
+++ views/403.php
@@ -0,0 +1,16 @@
+<?php require "partials/head.php"; ?>
+<?php require "partials/nav.php"; ?>
+
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1>You are not authorized to view this page.</h1>
+      <br>
+      <p>
+        <a href="/" class="text-blue-500 hover:underline">Return to Home Page</a>
+      </p>
+    </div>
+</main>
+
+
+<?php require "partials/footer.php"; ?>
---
+++ views/404.php
@@ -0,0 +1,16 @@
+<?php require "partials/head.php"; ?>
+<?php require "partials/nav.php"; ?>
+
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1>Sorry, the page you are looking for could not be found.</h1>
+      <br>
+      <p>
+        <a href="/" class="text-blue-500 hover:underline">Return to Home Page</a>
+      </p>
+    </div>
+</main>
+
+
+<?php require "partials/footer.php"; ?>
